0539f1d8b451b869ff3a9181df458af49e5e2355
Gina Choi <ginac@berkeley.edu>
2023-12-25 22:05:53 -0800

    getting the resume link to work

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -8,10 +8,9 @@ export default function Resume() {
       {/* REPLACE H1 ELEMENT BELOW WITH CODE FROM TODO */}
       <h1 className="title">resume</h1>
       <p>
-        resume here.
-        note: i've finished enough CS coursework to graduate as early as Dec 2024 (expected), 
-        and alternatively, as late as May 2025 if I attend all 4 years. 
-        these resumes are here to reflect that :)
+        Note: I've finished enough CS coursework to graduate as early as <b>Dec 2024</b> (expected), 
+        and alternatively, <br></br>as late as May 2025 if I attend all 4 years. 
+        These resumes are here to reflect that :)
       </p>
       
       
--- src/pages/work.js
+++ src/pages/work.js
@@ -9,11 +9,12 @@ export default function Work() {
       <h1 className="title">work</h1>
       {/* REPLACE OPENING P TAG BELOW WITH CODE FROM TODO */}
       <p>
-        i’ve previously been interviewed by ABC, CBS, and the SF Chronicle. my
-        work has also been featured on <a href="https://developers.facebook.com/blog/post/2020/12/21/reflecting-year-innovation-developer-creator-ecosystem/" target="_blank">Facebook Developers</a> and NBC Los Angeles.<br></br>
-        my portfolio can be found on my <a href="https://www.linkedin.com/in/ginac10/" target="_blank">Linkedin</a>.
+        i’ve previously been interviewed by ABC, CBS, and the SF Chronicle. <br></br>my
+        work has also been featured on <a href="https://developers.facebook.com/blog/post/2020/12/21/reflecting-year-innovation-developer-creator-ecosystem/" target="_blank">Facebook Developers</a> and NBC Los Angeles.
+        {/*<br></br>
+        my portfolio can be found on my <a href="https://www.linkedin.com/in/ginac10/" target="_blank">Linkedin</a>.*/}
       </p>
-      <p>featured projects</p>
+      <p><b>featured projects</b></p>
       <ul>
         <li>
           Proj 1
