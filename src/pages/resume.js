816983e047a82883d007ffb104af272e1891b094
Gina Choi <ginac@berkeley.edu>
2023-12-25 21:49:24 -0800

    resume, added links to home (from popsy)

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,28 +1,18 @@
 import * as React from "react";
-export default function Work() {
+export default function Resume() {
   /* DECLARE STYLE AND TRIGGER FOR WIGGLE EFFECT FROM TODO ON NEXT LINE */
 
   return (
     
     <div className="page">
       {/* REPLACE H1 ELEMENT BELOW WITH CODE FROM TODO */}
-      <h1 className="title">work</h1>
-      {/* REPLACE OPENING P TAG BELOW WITH CODE FROM TODO */}
+      <h1 className="title">resume</h1>
       <p>
-        i’ve previously been interviewed by ABC, CBS, and the SF Chronicle. my
-        work has also been featured on <a href="https://developers.facebook.com/blog/post/2020/12/21/reflecting-year-innovation-developer-creator-ecosystem/">Facebook Developers</a> and NBC Los Angeles.<br></br>
-        my portfolio can be found on my <a href="https://www.linkedin.com/in/ginac10/">Linkedin</a>.
+        resume here.
+        note: i've finished enough CS coursework to graduate as early as Dec 2024 (expected), 
+        and alternatively, as late as May 2025 if I attend all 4 years. 
+        these resumes are here to reflect that :)
       </p>
-      <p>featured projects</p>
-      <ul>
-        <li>
-          Proj 1
-        </li>
-        <li><strong>Proj 2</strong>
-        </li>
-        <li>Proj 3
-        </li>
-      </ul>
       
       
     </div>
